8e74fc235ea0789bfbee3ea9d6f09af5c8717ba6
Niklas Sombert <niklas@ytvwld.de>
2020-10-14 15:20:48 +0200

    Initial commit
    This is a minimal working version of a parser
    for command line arguments.

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+use std::vec::Vec;
+#[cfg(feature = "alloc")]
+use alloc::vec::Vec;
+
+/// Parse the command line.
+///
+/// This expects a slice of possible options and turns `-option foo` to `[("option", "foo")]`.
+/// Only `-key value` options are supported.
+///
+/// This function errors, if the command line options are not valid, see `ParseError` for details.
+#[cfg(any(feature = "std", feature = "alloc"))]
+pub fn parse<'a>(cmdline: &'a mut str, options: &[&'a str]) -> Result<Vec<(&'a str, &'a str)>, ParseError<'a>> {
+    let posix_parsed = cmdline_words_parser::parse_posix(cmdline);
+    let mut result = Vec::new();
+    let mut last = None;
+    // skip argv[0]
+    for arg in posix_parsed.skip(1) {
+        if let Some(l) = last {
+            // the last element was a key
+            result.push((l, arg));
+            last = None;
+        } else {
+            // the next element has to be a key
+            if let Some(a) = arg.strip_prefix("-") {
+                if options.contains(&a) {
+                    last = Some(a);
+                } else {
+                    return Err(ParseError::UnknownKey(a))
+                }
+            } else {
+                return Err(ParseError::NotAKey(arg))
+            }
+        }
+    }
+    Ok(result)
+}
+
+
+#[derive(Debug)]
+/// Errors occurred during parsing the command line.
+pub enum ParseError<'a> {
+    /// expected a key, but argument didn't start with a dash
+    NotAKey(&'a str),
+    /// key is not accepted
+    UnknownKey(&'a str),
+    /// the default error
+    Unknown,
+}
